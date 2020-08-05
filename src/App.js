da57536e8de350c68e6a7e945af82281959ef454
Beth Jackson <beth.jack@gmail.com>
2020-08-05 14:59:55 +0200

    Updated background New background and other tinkering.

--- src/App.js
+++ src/App.js
@@ -4,29 +4,37 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <div>
-        <header className="header">
-          <h1 className="header-h1">cph-kiwi</h1>
-
-          <nav id="navbar" className="menu">
-            <ul class="nav-list">
-              <li>Portfolio</li>
-              <li>About</li>
-              <li>Resume</li>
-              <li>Contact</li>
+      <header className="main-header">
+        <div class="container">
+          <h1 class="main-header-logo">B.</h1>
+          <nav class="main-nav">
+            <ul class="main-nav-list">
+              <li>
+                <a>Portfolio</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+              <li>
+                <a>Resume</a>
+              </li>
+              <li>
+                <a>Contact</a>
+              </li>
             </ul>
           </nav>
-        </header>
-        <div>
-          <h2 className="body-h2">Hi, I'm Beth.</h2>
-          <h3 className="body-h3">Web designer and developer.</h3>
-          <p className="description">
-            Currently based in Copenhagen, Denmark, studying Danish and web
-            development. I have focused on HTML, CSS, Javascript and React to
-            start with, and continue to study and work on private projects.
-          </p>
-          <label className="check-out-work-label">Check out my work</label>
         </div>
+      </header>
+
+      <div>
+        <h2 className="body-h2">Hi, I'm Beth.</h2>
+        <h3 className="body-h3">Web developer.</h3>
+        <p className="description">
+          Currently based in Copenhagen, Denmark, studying Danish and web
+          development. I have focused on HTML, CSS, Javascript and React to
+          start with, and continue to study and work on private projects.
+        </p>
+        <label className="check-out-work-label">Check out my work</label>
       </div>
     </div>
   );
