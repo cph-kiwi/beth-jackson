05e94a52f71e4724b43b9846829495ecd27bd85d
Beth Jackson <beth.jack@gmail.com>
2020-08-06 20:13:56 +0200

    Daily updates

--- src/App.js
+++ src/App.js
@@ -4,37 +4,57 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
+      <a id="top" />
       <header className="main-header">
-        <div class="container">
-          <h1 class="main-header-logo">B.</h1>
-          <nav class="main-nav">
-            <ul class="main-nav-list">
-              <li>
-                <a>Portfolio</a>
-              </li>
-              <li>
-                <a>About</a>
-              </li>
-              <li>
-                <a>Resume</a>
-              </li>
-              <li>
-                <a>Contact</a>
-              </li>
-            </ul>
-          </nav>
+        <div className="main-header-logo">
+          <a href="#top">B.</a>
         </div>
+        <nav className="main-nav">
+          <ul className="main-nav-list">
+            <li>
+              <a href="#">Portfolio</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Resume</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
       </header>
-
-      <div>
-        <h2 className="body-h2">Hi, I'm Beth.</h2>
-        <h3 className="body-h3">Web developer.</h3>
-        <p className="description">
-          Currently based in Copenhagen, Denmark, studying Danish and web
-          development. I have focused on HTML, CSS, Javascript and React to
-          start with, and continue to study and work on private projects.
-        </p>
-        <label className="check-out-work-label">Check out my work</label>
+      <div className="intro">
+        <div className="intro-container">
+          <h1 className="intro-h1">Hi, I'm Beth.</h1>
+          <h2 className="intro-h2">Web developer.</h2>
+          <p className="intro-p">
+            Currently based in Copenhagen, Denmark, studying Danish and web
+            development. I have focused on HTML, CSS, Javascript and React to
+            start with, and continue to study and work on private projects.
+          </p>
+          <div className="intro-link-to-more">
+            <a href="#page2">Check out my work</a>
+          </div>
+        </div>
+      </div>
+      <div className="page2" id="page2">
+        <div className="page2-container">
+          <h5 className="page2-h5">Featured projects</h5>
+          <h3 className="page2-h3">Pig Latin Translator</h3>
+          <p className="page2-p">
+            My first personal project. An app that translates from English into
+            Pig Latin (a fun, pseudo-language, mostly used by children in play).
+          </p>
+          <div className="page2-link-to-more">
+            <a href="https://pig-latin.cph.kiwi/">Visit Pig Latin Translator</a>
+          </div>
+        </div>
       </div>
     </div>
   );
