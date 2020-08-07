96c8bcf3b4759d27c721b6a4378b8363e62796f6
Beth Jackson <beth.jack@gmail.com>
2020-08-07 12:05:41 +0200

    Daily updates

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ export default function App() {
               <a href="#page2">Portfolio</a>
             </li>
             <li>
-              <a href="#">About</a>
+              <a href="#about">About</a>
             </li>
             <li>
               <a href="#">Resume</a>
@@ -43,7 +43,7 @@ export default function App() {
               <a href="#">Blog</a>
             </li>
             <li>
-              <a href="#">Contact</a>
+              <a href="#contact">Contact</a>
             </li>
           </ul>
         </nav>
@@ -72,6 +72,45 @@ export default function App() {
           </p>
           <div className="page2-link-to-more">
             <a href="https://pig-latin.cph.kiwi/">Visit Pig Latin Translator</a>
+          </div>
+        </div>
+      </div>
+      <div className="about" id="about">
+        <div className="about-container">
+          <h5 className="about-h5">About me</h5>
+          <h3 className="about-h3">Beth Jackson</h3>
+          <p className="about-p">
+            I grew up in rural New Zealand, on a farm in Taranaki. I attended
+            the University of Otago, where I studied psychology, philosophy, and
+            human resources management. After completing my studies, I worked at
+            the University of Otago as a librarian, and then as an
+            administrative assistant. I worked in many different roles within
+            university administration, at both the University of Otago in
+            Dunedin and UCL in London. I lived in London for 7 years, and then
+            moved to Copenhagen. Currently I am studying web development and
+            Danish, and looking for work in the tech sector.
+          </p>
+        </div>
+      </div>
+      <div className="contact" id="contact">
+        <div className="contact-container">
+          <h5 className="contact-h5">Contact me</h5>
+          <h3 className="contact-h3">beth.jack@gmail.com</h3>
+          <p className="contact-p">
+            I am always keen to collaborate. I'm hoping to find a permanent
+            position in the tech sector. Let's get in touch.
+          </p>
+          <div className="social-container">
+            <a
+              href="https://www.linkedin.com/in/beth-jackson-4a29ab193/"
+              target="_blank"
+            >
+              {" "}
+              <img
+                src="https://yt3.ggpht.com/-CepHHHB3l1Y/AAAAAAAAAAI/AAAAAAAAAAA/Z8MftqWbEqA/s900-c-k-no/photo.jpg"
+                alt="linkedin logo"
+              />{" "}
+            </a>
           </div>
         </div>
       </div>
