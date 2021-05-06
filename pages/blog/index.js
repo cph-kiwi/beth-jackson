7e0bc2484187dd33ee7d5abfd3e5b616b8ae2736
Beth Jackson <beth.jack@gmail.com>
2021-05-06 12:14:21 +0200

    Changed opening paragraph

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -25,8 +25,15 @@ export default function Blog() {
       <div className="blog-main">
         <div className="blog-container">
           <h1 className="blog-h1">Beth's blog.</h1>
-          <h2 className="blog-h2">
-            My web development journey from the very beginning.
+          <h2 className="blog-p">
+            I have a terrible memory. I make pancakes every weekend, and I still
+            have to look up the recipe. After reading a whole bunch of
+            organisational books, I have discovered that this isn't a bad thing.
+            By writing everything down and not trying to memorise everything, I
+            am reducing cognitive load. In any case, I can't remember how to
+            make porridge from one winter to the next, so I'm writing all my
+            recipes here. This is more for me than anyone else, but happy
+            browsing.
           </h2>
           <h3 className="blog-h3">Blog entry title</h3>
           <p className="blog-p">Blog text</p>
