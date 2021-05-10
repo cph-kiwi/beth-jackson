133a75670dc91ef1a94a0adff293be28c22a55e9
Beth Jackson <beth.jack@gmail.com>
2021-05-10 14:21:56 +0200

    Got links hooked up on blog index page

--- src/blog-support-helpers.js
+++ src/blog-support-helpers.js
@@ -8,7 +8,10 @@ export function getBlogBySlug(slug) {
   const fullPath = join(blogDirectory, `${slug}.mdx`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
-  return { data: { ...data, published: data.published.toJSON() }, content };
+  return {
+    data: { ...data, published: data.published.toJSON(), slug },
+    content,
+  };
 }
 
 export function getAllPosts() {
