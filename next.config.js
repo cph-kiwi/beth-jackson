9d7d6b35f5116802553ed8dab7b9ca62d8322b80
Beth Jackson <bj@silvi.ai>
2024-07-17 10:03:09 +0200

    Updated node and build config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
+	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+		// Note: we provide webpack above so you should not `require` it
+		// Perform customizations to webpack config
 
-    // Important: return the modified config
-    config.node = {
-      fs: "empty",
-    };
-    return config;
-  },
+		// Important: return the modified config
+
+		return config;
+	},
 };
